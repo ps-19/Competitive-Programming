1e022190d04246a1935998844eaa813f83cc89a2
Priyansh Singh <63330165+ps-19@users.noreply.github.com>
2022-01-23 13:02:31 +0530

    Create lps(KMP).cpp

---
+++ lps(KMP).cpp
@@ -0,0 +1,32 @@
+void LPS(string txt,vector<int> &lps){
+    lps[0] = 0;                   
+    int len = 0;
+    int i=1;
+    while (i<txt.length()){
+        if(txt[i]==txt[len]){   
+            len++;
+            lps[i] = len;
+            i++;
+            continue;
+        }
+        else{                   
+            if(len==0){         
+                lps[i] = 0;
+                i++;
+                continue;
+            }
+            else{              
+                len = lps[len-1];
+                continue;
+            }
+        }
+    }
+}
+
+int main(){
+  string pattern;
+  cin>>pattern;
+  vector<int> lps(pattern.length());
+  LPS(pattern,lps);
+  return 0;
+}
