b72015efdb526369a49849da6cb3396731d38da7
Priyansh Singh <priyansh.singh100@gmail.com>
2022-05-05 18:15:06 +0530

    Update TreeStartingCode.cpp

--- TreeStartingCode.cpp
+++ TreeStartingCode.cpp
@@ -6,3 +6,26 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
+
+
+//***************************************************************************************************************//
+
+class node {
+public:
+    char value;
+    node* left;
+    node* right;
+    node* next = NULL;
+    node(char c)
+    {
+        this->value = c;
+        left = NULL;
+        right = NULL;
+    }
+    node()
+    {
+        left = NULL;
+        right = NULL;
+    }
+    
+};
